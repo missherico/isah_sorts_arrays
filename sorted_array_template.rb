c9909b0dfa267fb4fd92bd18129efe54c01b7db9
Isa Herico <miss.herico@gmail.com>
2014-01-13 17:52:51 -0800

    added input_arr.each method in initialize

--- sorted_array_template.rb
+++ sorted_array_template.rb
@@ -3,12 +3,16 @@ class SortedArray
 
   def initialize(input_arr=[])
     @internal_arr = []
+    input_arr.each do |element|
+      self.add(element)
+    end
     # Fill in the rest of the initialize method here.
     # What should you do with each element of the incoming array?
   end
 
   def add(new_ele)
     # Hint: Use the Array#insert method.
+
     raise NotImplementedError.new("You need to implement the add method!")
   end
 
