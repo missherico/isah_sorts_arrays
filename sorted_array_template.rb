8035c04fc68319628eab3f2e89ad306b3d27998f
Isa Herico <miss.herico@gmail.com>
2014-01-13 19:30:27 -0800

    validated index & add methods

--- sorted_array_template.rb
+++ sorted_array_template.rb
@@ -3,17 +3,17 @@ class SortedArray
 
   def initialize(input_arr=[])
     @internal_arr = []
-    input_arr.each do |element|
-      self.add(element)
-    end
+    input_arr.each { |x| add x}
+
     # Fill in the rest of the initialize method here.
     # What should you do with each element of the incoming array?
   end
 
   def add(new_ele)
     # Hint: Use the Array#insert method.
+    index = first_larger_index(new_ele)
+    @internal_arr.insert( index, new_ele)    
 
-    raise NotImplementedError.new("You need to implement the add method!")
   end
 
   def size
@@ -25,11 +25,56 @@ class SortedArray
   end
 
   def first_larger_index(target, start_ind=0, end_ind=@internal_arr.size)
-    #binary search
-    raise NotImplementedError.new("You need to implement the first_larger_index method!")
+    if start_ind >= end_ind
+      return end_ind
+    end
+    
+    mid_index = ((end_ind + start_ind) / 2)
+    mid_element = @internal_arr[mid_index]
+
+    if mid_element  == target
+      return mid_index
+    elsif mid_element > target
+      return first_larger_index(target, start_ind, mid_index)
+    elsif mid_element < target
+      return first_larger_index(target, (mid_index + 1), end_ind)
+    end
   end
 
-  def index(target)
-    raise NotImplementedError.new("You need to implement the index method!")
+
+
+  def index(target, start_ind = 0, end_ind = @internal_arr.size)
+
+  if start_ind >= end_ind
+    return nil
+    end
+    mid_index = (end_ind - start_ind) / 2 + start_ind
+    middle_element = @internal_arr[mid_index]
+    if middle_element == target
+      return mid_index
+    elsif middle_element > target
+        return index(target, start_ind, mid_index)
+    else
+        return index(target, mid_index + 1, end_ind)
+    end
   end
+  # def index(target, start_ind = 0, end_ind = @internal_arr.size)
+    
+  #   if start_ind >= end_ind
+  #     return nil 
+  #   end
+    
+  #   mid_index = ((end_ind + start_ind) / 2) + start_ind
+  #   mid_element = @internal_arr[mid_index]
+
+  #   if mid_element == target
+  #     return mid_index
+  #   elsif mid_element > target
+  #     return index(target, start_ind, mid_index)
+  #   elsif mid_element < target
+  #     return index(target, mid_index + 1, end_ind)
+  #   end
+
+  # end
+
 end
